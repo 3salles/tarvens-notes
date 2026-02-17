63037ea0e4af831db939e02d1c36134c51130bbc
Beatriz Salles <beatrizsallesss@gmail.com>
2026-02-17 20:57:42 -0300

    feat: ✨ Create Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,14 @@
 export default function Home() {
-  return <h1>Taverns & Notes</h1>;
+  return (
+    <section className="flex min-h-full">
+      <div className="flex flex-1 items-center justify-center">
+        <div className="text-center">
+          <h1 className="text-2xl font-bold mb-4">Selecione uma sessão</h1>
+          <p className="text-gray-400">
+            Escolha uma sessão da lista ao lado para visualizar e editar
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
