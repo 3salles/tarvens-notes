82e2c4d366a400b01755682bb1948b2c90bde75a
Beatriz Salles <beatrizsallesss@gmail.com>
2026-03-03 20:38:56 -0300

    test: ✅ Add update session test in session-form test

--- src/tests/infra/components/session/session-form.spec.tsx
+++ src/tests/infra/components/session/session-form.spec.tsx
@@ -1,4 +1,4 @@
-import { SessionForm } from '@/components/session';
+import { SessionForm, SessionFormProps } from '@/components/session';
 import { render, screen } from '@/lib/test-utils';
 import userEvent from '@testing-library/user-event';
 import { toast } from 'sonner';
@@ -9,16 +9,18 @@ jest.mock('next/navigation', () => ({
 }));
 
 const createActionMock = jest.fn();
+const updateActionMock = jest.fn();
 jest.mock('@/app/actions/session.actions', () => ({
   createSessionAction: (...args: unknown[]) => createActionMock(...args),
+  updateSessionAction: (...args: unknown[]) => updateActionMock(...args),
 }));
 
 jest.mock('sonner', () => ({
   toast: { success: jest.fn(), error: jest.fn() },
 }));
 
-const renderSut = () => {
-  return render(<SessionForm />);
+const renderSut = ({ session }: SessionFormProps = {} as SessionFormProps) => {
+  return render(<SessionForm session={session} />);
 };
 
 describe('SessionForm', () => {
@@ -26,6 +28,7 @@ describe('SessionForm', () => {
 
   beforeEach(() => {
     createActionMock.mockReset();
+    updateActionMock.mockReset();
     refreshMock.mockReset();
     (toast.success as jest.Mock).mockReset();
     (toast.error as jest.Mock).mockReset();
@@ -88,5 +91,46 @@ describe('SessionForm', () => {
     expect(screen.getByText('Título é obrigatório')).toBeVisible();
     expect(screen.getByText('Conteúdo é obrigatório')).toBeVisible();
     expect(createActionMock).not.toHaveBeenCalled();
+  });
+
+  it('should successfully update a session', async () => {
+    updateActionMock.mockResolvedValue({
+      success: true,
+      message: 'Sessão atualizada com sucesso!',
+    });
+    const now = new Date();
+    const session = {
+      id: '1',
+      title: 'old',
+      note: 'old',
+      createdAt: now,
+      updatedAt: now,
+      sessionDate: now,
+    };
+
+    renderSut({ session });
+
+    const titleInput = screen.getByPlaceholderText('Título da sessão');
+    await user.clear(titleInput);
+    await user.type(titleInput, 'New');
+
+    const noteInput = screen.getByPlaceholderText(
+      'Digite o conteúdo da sessão...'
+    );
+    await user.clear(noteInput);
+    await user.type(noteInput, 'New note');
+
+    const submitButton = screen.getByRole('button', { name: 'Salvar' });
+    await user.click(submitButton);
+
+    expect(updateActionMock).toHaveBeenCalledWith({
+      id: session.id,
+      title: 'New',
+      note: 'New note',
+    });
+    expect(toast.success).toHaveBeenCalledWith(
+      'Sessão atualizada com sucesso!'
+    );
+    expect(refreshMock).toHaveBeenCalledTimes(1);
   });
 });
